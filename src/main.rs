a68cf8aa69766b9825310b92e2579194375b696b
bana118 <bana.titech@gmail.com>
2020-12-03 14:11:43 +0000

    add simple form

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,49 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{error, web, App, Error, HttpResponse, HttpServer};
+use serde::{Deserialize, Serialize};
+use std::str;
+use tera::{Context, Tera};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+async fn greet() -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body("Hello, world!"))
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct FormParams {
+    input: String,
+}
+
+async fn forms(tmpl: web::Data<Tera>) -> Result<HttpResponse, Error> {
+    let mut ctx = Context::new();
+    ctx.insert("input", &"");
+    let view = tmpl
+        .render("forms.html", &ctx)
+        .map_err(|e| error::ErrorInternalServerError(e))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(view))
+}
+
+async fn simple_form(
+    params: web::Form<FormParams>,
+    tmpl: web::Data<Tera>,
+) -> Result<HttpResponse, Error> {
+    let mut ctx = Context::new();
+    ctx.insert("input", &params.input);
+    let view = tmpl
+        .render("forms.html", &ctx)
+        .map_err(|e| error::ErrorInternalServerError(e))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(view))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let templates = Tera::new("templates/**/*").unwrap();
+    HttpServer::new(move || {
         App::new()
-            .route("/hello", web::get().to(greet))
-            .route("/hello/{name}", web::get().to(greet))
+            .data(templates.clone())
+            .route("/", web::get().to(greet))
+            .route("/forms", web::get().to(forms))
+            .route("/forms/simple", web::post().to(simple_form))
     })
     .bind("127.0.0.1:8080")?
     .run()
